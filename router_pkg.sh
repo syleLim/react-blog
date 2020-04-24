3e3132bae392399d6719892fdbd6d775a237fd77
syleLim <syle6492@gmail.com>
2020-04-24 00:10:17 -0700

    set first defulat

--- router_pkg.sh
+++ router_pkg.sh
@@ -2,6 +2,8 @@ npm install --save-dev react-router-dom
 
 cd src
 rm index.js
+rm index.html
+mkdir pages
 
 echo "import React from 'react';
 import ReactDOM from 'react-dom';
@@ -27,12 +29,13 @@ export default Root;
 
 echo 'import React from "react"
 import { Route } from "react-router-dom"
-
+import Home from "./pages/Home"
 class App extends React.Component {
     render () {
         return (
             <div>
                 Main
+                <Route exact path="/" component={Home} />
             </div>
         )
     };
@@ -40,6 +43,34 @@ class App extends React.Component {
 
 export default App;
 ' >> App.js
+
+'<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My React App</title>
+</head>
+<body>
+    <div id="root"></div>				
+</body>
+</html>
+' >> index.html
+
+echo 'import React from "react"
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home</h2>
+            <p>Home Page</p>
+        </div>
+    )
+}
+' >> ./pages/Home.js
+
+export default Home;
 
 rm ./component/App.js
 cd ..
